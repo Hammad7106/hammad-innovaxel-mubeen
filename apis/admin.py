57c594a7c5b88c956bddd5cd81673e24cb45f8f8
Hammad <Hammad7106>
2025-03-16 12:46:33 +0500

    Created Superuser for admin operations

--- apis/admin.py
+++ apis/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import ShortURL
+
+@admin.register(ShortURL)
+class ShortenedURLAdmin(admin.ModelAdmin):
+    list_display = ('original_url', 'short_code', 'created_at', 'updated_at', 'access_count')
+    search_fields = ('url', 'short_code')
+    list_filter = ('created_at', 'updated_at')
